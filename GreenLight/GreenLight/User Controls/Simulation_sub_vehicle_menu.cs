3e82e86aa7ee0fccad183197f997d3db684d711f
VNDanny <danny_bui@hotmail.com>
2020-12-08 10:17:24 +0100

    Positioning

--- GreenLight/GreenLight/User Controls/Simulation_sub_vehicle_menu.cs
+++ GreenLight/GreenLight/User Controls/Simulation_sub_vehicle_menu.cs
@@ -35,43 +35,43 @@ namespace GreenLight
             Selection_box.Location = new Point(3, 35);
             this.Controls.Add(Selection_box);
 
-            Slider Max_speed = new Slider(new Point(25, 260), 0, 100, 10);
+            Slider Max_speed = new Slider(new Point(25, 360), 0, 100, 10);
             this.Controls.Add(Max_speed);
-            SliderText Max_speed_label = new SliderText(Dosis_font_family, new Point(25, 240), "Max speed:");
+            SliderText Max_speed_label = new SliderText(Dosis_font_family, new Point(25, 340), "Max speed:");
             this.Controls.Add(Max_speed_label);
-            SliderText Max_speed_Value = new SliderText(Dosis_font_family, new Point(125, 240), Max_speed.Value.ToString() + " km/h");
+            SliderText Max_speed_Value = new SliderText(Dosis_font_family, new Point(125, 340), Max_speed.Value.ToString() + " km/h");
             this.Controls.Add(Max_speed_Value);
             Max_speed.ValueChanged += (object o, EventArgs EA) => { Max_speed_Value.Text = Max_speed.Value.ToString() + " km/h"; };
 
-            Slider Length = new Slider(new Point(25, 220), 0, 100, 10);
+            Slider Length = new Slider(new Point(25, 320), 0, 100, 10);
             this.Controls.Add(Length);
-            SliderText Length_label = new SliderText(Dosis_font_family, new Point(25, 200), "Length:");
+            SliderText Length_label = new SliderText(Dosis_font_family, new Point(25, 300), "Length:");
             this.Controls.Add(Length_label);
-            SliderText Length_Value = new SliderText(Dosis_font_family, new Point(125, 200), Length.Value.ToString() + " m");
+            SliderText Length_Value = new SliderText(Dosis_font_family, new Point(125, 300), Length.Value.ToString() + " m");
             this.Controls.Add(Length_Value);
             Length.ValueChanged += (object o, EventArgs EA) => { Length_Value.Text = Length.Value.ToString() + " m"; };
 
-            Slider Acceleration = new Slider(new Point(25, 180), 0, 100, 10);
+            Slider Acceleration = new Slider(new Point(25, 280), 0, 100, 10);
             this.Controls.Add(Acceleration);
-            SliderText Acceleration_label = new SliderText(Dosis_font_family, new Point(25, 160), "Acceleration:");
+            SliderText Acceleration_label = new SliderText(Dosis_font_family, new Point(25, 260), "Acceleration:");
             this.Controls.Add(Acceleration_label);
-            SliderText Acceleration_Value = new SliderText(Dosis_font_family, new Point(125, 160), Acceleration.Value.ToString() + " m/s^2");
+            SliderText Acceleration_Value = new SliderText(Dosis_font_family, new Point(125, 260), Acceleration.Value.ToString() + " m/s^2");
             this.Controls.Add(Acceleration_Value);
             Acceleration.ValueChanged += (object o, EventArgs EA) => { Acceleration_Value.Text = Acceleration.Value.ToString() + " m/s^2"; };
 
-            Slider Weight = new Slider(new Point(25, 140), 0, 100, 10);
+            Slider Weight = new Slider(new Point(25, 240), 0, 100, 10);
             this.Controls.Add(Weight);
-            SliderText Weight_label = new SliderText(Dosis_font_family, new Point(25, 120), "Weight:");
+            SliderText Weight_label = new SliderText(Dosis_font_family, new Point(25, 220), "Weight:");
             this.Controls.Add(Weight_label);
-            SliderText Weight_Value = new SliderText(Dosis_font_family, new Point(125, 160), Weight.Value.ToString() + " kg");
+            SliderText Weight_Value = new SliderText(Dosis_font_family, new Point(125, 220), Weight.Value.ToString() + " kg");
             this.Controls.Add(Weight_Value);
             Weight.ValueChanged += (object o, EventArgs EA) => { Weight_Value.Text = Weight.Value.ToString() + " kg"; };
 
-            Slider Occurunce = new Slider(new Point(25, 100), 0, 100, 10);
+            Slider Occurunce = new Slider(new Point(25, 200), 0, 100, 10);
             this.Controls.Add(Occurunce);
-            SliderText Occurunce_label = new SliderText(Dosis_font_family, new Point(25, 80), "Occurunce:");
+            SliderText Occurunce_label = new SliderText(Dosis_font_family, new Point(25, 180), "Occurunce:");
             this.Controls.Add(Occurunce_label);
-            SliderText Occurunce_Value = new SliderText(Dosis_font_family, new Point(125, 80), Occurunce.Value.ToString() + " %");
+            SliderText Occurunce_Value = new SliderText(Dosis_font_family, new Point(125, 180), Occurunce.Value.ToString() + " %");
             this.Controls.Add(Occurunce_Value);
             Occurunce.ValueChanged += (object o, EventArgs EA) => { Occurunce_Value.Text = Occurunce.Value.ToString() + " %"; };
 
