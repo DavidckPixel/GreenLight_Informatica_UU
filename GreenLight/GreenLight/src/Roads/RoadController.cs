e3282700b8f7388180abd7cd27715dc3513dca85
Marc de Jong <m.dejong14@students.uu.nl>
2021-01-21 20:20:09 +0100

    Settingscreen given priority

--- GreenLight/GreenLight/src/Roads/RoadController.cs
+++ GreenLight/GreenLight/src/Roads/RoadController.cs
@@ -344,7 +344,7 @@ namespace GreenLight
                 DrivingLaneHitbox();
             }
 
-            settingScreen.Show();
+            settingScreen.ShowDialog();
             settingScreen.BringToFront();
             settingScreen.Invalidate();
         }
